e3f091d2400e613bfb8ea2243c5c16ef0f02d65a
Roshan <roshanbhuvad15@gmail.com>
2020-07-30 17:22:17 +0530

    Database connection done

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
-const PORT = 5000
+const mongoose = require("mongoose");
+const PORT = 5000;
+const { MONGOURI } = require("./keys");
+//EUEpULhlQOcFmXuy
+require("./models/user");
+mongoose.model("User");
 
-app.get("/", (req, res) => {
-    res.send("hello world")
-})
+mongoose.connect(MONGOURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", () => {
+  console.log("connected to mongo yeah");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("err connecting", err);
+});
 
 app.listen(PORT, () => {
-    console.log("Server is running on", PORT)
+  console.log("Server is running on", PORT);
 });
---
+++ keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGOURI:
+    "mongodb+srv://roshan:EUEpULhlQOcFmXuy@cluster0.opo8m.mongodb.net/<dbname>?retryWrites=true&w=majority",
+};

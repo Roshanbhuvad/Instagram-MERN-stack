d90a8592f67fade468ec7a680e9bdbea9d7669f1
Roshan <roshanbhuvad15@gmail.com>
2020-07-30 19:03:36 +0530

    Token authentication added

--- keys.js
+++ keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   MONGOURI:
     "mongodb+srv://roshan:EUEpULhlQOcFmXuy@cluster0.opo8m.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  JWT_SECRET: "300spartaTransformer152896",
 };
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const User = mongoose.model("User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../keys");
+
 /*router.get("/", (req, res) => {
   res.send("hello");
 }); */
@@ -55,6 +57,8 @@ router.post("/signin", (req, res) => {
       .then((doMatch) => {
         if (doMatch) {
           //res.json({ message: "Successfully logged in" });
+          const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET);
+          res.json({ token });
         } else {
           return res.send(422).json({ error: "Invalid Email or Password" });
         }

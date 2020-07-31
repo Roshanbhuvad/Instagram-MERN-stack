7937c26f8fb13a47c73a9cd9352a4684bc5b8152
Roshan <roshanbhuvad15@gmail.com>
2020-07-31 17:31:32 +0530

    Profile and home pages added

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,7 +1,61 @@
 import React from "react";
 
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div className="home">
+      <div className="card home-card">
+        <h5>Roshan</h5>
+        <div className="card-image">
+          <img
+            src="https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            alt=""
+          />
+        </div>
+        <div className="card-content">
+          <i className="material-icons" style={{ color: "red" }}>
+            favorite
+          </i>
+          <h6>Title</h6>
+          <p>Just feel the nature beauty </p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
+      <div className="card home-card">
+        <h5>Roshan</h5>
+        <div className="card-image">
+          <img
+            src="https://images.unsplash.com/photo-1461301214746-1e109215d6d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            alt=""
+          />
+        </div>
+        <div className="card-content">
+          <i className="material-icons" style={{ color: "red" }}>
+            favorite
+          </i>
+          <h6>Title</h6>
+          <p>Just feel the nature beauty </p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
+      <div className="card home-card">
+        <h5>Roshan</h5>
+        <div className="card-image">
+          <img
+            src="https://images.unsplash.com/photo-1558981285-6f0c94958bb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            alt=""
+          />
+        </div>
+        <div className="card-content">
+          <i className="material-icons" style={{ color: "red" }}>
+            favorite
+          </i>
+          <h6>Title</h6>
+          <p>Just feel the nature beauty </p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;

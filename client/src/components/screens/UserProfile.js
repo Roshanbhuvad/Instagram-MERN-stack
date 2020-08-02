18122234841a4ff5d6de6444db371607de2b261c
Roshan <roshanbhuvad15@gmail.com>
2020-08-02 16:52:22 +0530

    Working on followers & following

--- client/src/components/screens/UserProfile.js
+++ client/src/components/screens/UserProfile.js
@@ -15,10 +15,40 @@ const Profile = () => {
     })
       .then((res) => res.json())
       .then((result) => {
-        console.log(result);
+        //console.log(result);
+
         setProfile(result);
       });
   }, []); // I am passing empty dependency array because I want to get this fire when component is mounted first
+
+  const followUser = () => {
+    fetch("/follow", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        followId: userid,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        //console.log(data);
+        dispatch({
+          type: "UPDATE",
+          payload: { following: data.following, followers: data.followers },
+        });
+        localStorage.setItem("user", JSON.stringify(data));
+        setProfile((prevState) => {
+          return {
+            ...prevState,
+            user: data,
+          };
+        });
+      });
+  };
+
   return (
     <>
       {userProfile ? (
@@ -53,9 +83,15 @@ const Profile = () => {
                 }}
               >
                 <h6>{userProfile.posts.length} posts</h6>
-                <h6>40 followers</h6>
-                <h6>40 following</h6>
+                <h6>{userProfile.user.followers.length} followers</h6>
+                <h6>{userProfile.user.following.length} following</h6>
               </div>
+              <button
+                className="btn waves-effect waves-light #64b5f6 blue darken-1"
+                onClick={() => followUser()}
+              >
+                Follow
+              </button>
             </div>
           </div>
 
